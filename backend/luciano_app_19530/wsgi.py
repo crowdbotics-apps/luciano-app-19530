35eebe62814dc5a1ca4a4b3ef2a53b0748d52fc5
Crowdbotics <team@crowdbotics.com>
2020-08-12 21:02:12 +0000

    Update models

--- backend/luciano_app_19530/wsgi.py
+++ backend/luciano_app_19530/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luciano_app_19530.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luciano_app_19530.settings")
 
 application = get_wsgi_application()
